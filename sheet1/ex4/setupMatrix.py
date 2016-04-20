d13d746617bb8891a625aafdc6996284cfbe7553
Amir Najafgholi <s6amnaja@uni-bonn.de>
2016-04-20 09:24:47 +0200

    a correction

--- sheet1/ex4/setupMatrix.py
+++ sheet1/ex4/setupMatrix.py
@@ -1,12 +1,13 @@
 import numpy as np
 
-def matrix(T,w):
-    N = len(T)
+def matrix(Tin,w):
+    M = Tin.copy()
+    N = len(M)
 
     for i in range(N):
         for j in range(N):
-            T[i,j] = w*T[i,j]*delta(i+1,j+1) + delta(i+1,j+2) + delta(i+2,j+1)
-    return T
+            M[i,j] = w*M[i,j]*delta(i+1,j+1) + delta(i+1,j+2) + delta(i+2,j+1)
+    return M
 
 
 def delta(i, j):
